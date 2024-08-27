e950fbe346456043293119e3faa09558521dc9a1
Samvel <loidnoir@gmail.com>
2024-08-28 02:19:16 +0400

    fix: dm ignore

--- src/events/message.ts
+++ src/events/message.ts
@@ -5,11 +5,12 @@ import ask from '@utils/AskGPT'
 const messageEvent = new Event('messageCreate').setLogic(async (client, message) => {
     if (message.author.bot) return
     if (!Config.isUser(message.author) && !Config.isAdmin(message.author.id)) return
+    if (!message.channel.isDMBased()) {
+        const aliases = Config.getConfig().system.aliases
+        const isMessageContainAlias = message.content.split(' ').some(word => aliases.includes(word))
 
-    const aliases = Config.getConfig().system.aliases
-    const isMessageContainAlias = message.content.split(' ').some(word => aliases.includes(word))
-
-    if (!isMessageContainAlias || message.channel.isDMBased()) return
+        if (!isMessageContainAlias) return
+    }
 
     await message.channel.sendTyping().then(async a => {
         const response = await ask(client, message.author.id, message.content)
