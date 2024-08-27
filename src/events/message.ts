ee73d03e4e3f23b5ec002209e7364f0ca3cf3d66
Samvel <loidnoir@gmail.com>
2024-08-28 02:47:43 +0400

    feat: gpt reply

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,21 +1,35 @@
 import Config from '@constants/Config'
+import Client from '@structures/Client'
 import Event from '@structures/Event'
 import ask from '@utils/AskGPT'
+import { Message } from 'discord.js'
 
 const messageEvent = new Event('messageCreate').setLogic(async (client, message) => {
     if (message.author.bot) return
     if (!Config.isUser(message.author) && !Config.isAdmin(message.author.id)) return
+
+    if (message.reference) {
+        if (!message.reference.messageId) return
+        const originalMessage = await message.channel.messages.fetch(message.reference.messageId)
+
+        if (originalMessage.author.id == client.user?.id) {
+            replyMessage(client, message, `Replying to this: "${originalMessage.content}"`)
+        }
+    }
+
     if (!message.channel.isDMBased()) {
         const aliases = Config.getConfig().system.aliases
         const isMessageContainAlias = message.content.split(' ').some(word => aliases.includes(word))
 
-        if (!isMessageContainAlias) return
+        if (isMessageContainAlias) replyMessage(client, message)
     }
+})
 
+async function replyMessage(client: Client, message: Message, context?: string) {
     await message.channel.sendTyping().then(async a => {
-        const response = await ask(client, message.author.id, message.content)
+        const response = await ask(client, message.author.id, message.content, context)
         await message.reply({ content: response ?? 'Something went wrong', allowedMentions: { repliedUser: false } })
     })
-})
+}
 
 export default messageEvent
